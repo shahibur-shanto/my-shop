a474e368bc0fb9a847297fea2d9775d34e01fb37
shahibur-shanto <shahibur407@example.com>
2025-09-23 00:24:42 +0600

    some fixes to add to cart

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -70,7 +70,7 @@ const handleAddToCart = (product) => {
         <nav class="bg-green-100 shadow-md relative">
             <div class="container mx-auto px-6 py-4 flex justify-between items-center">
                 <!-- Brand -->
-                <Link href="/" class="text-2xl font-bold tracking-tight">
+                <Link :href="route('home')" class="text-2xl font-bold tracking-tight">
           <span
               class="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-pink-500 to-sky-500"
               aria-label="NoumiMart"
@@ -88,12 +88,12 @@ const handleAddToCart = (product) => {
                 <!-- Auth Buttons Desktop -->
                 <div class="hidden md:flex items-center space-x-2">
                     <div class="relative">
-                        <button @click="cartOpen = !cartOpen" class="text-gray-700 hover:text-gray-900 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <button @click="cartOpen = !cartOpen" class="pr-4 text-gray-700 hover:text-gray-900 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
                         </button>
-                        <span v-if="totalItems > 0" class="absolute -top-2 -right-2 bg-red-600 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
+                        <span v-if="totalItems > 0" class="absolute -top-2 -right-1.5 bg-red-600 text-white rounded-full text-xs w-6 h-6 flex items-center justify-center">
             {{ totalItems }}
         </span>
                     </div>
